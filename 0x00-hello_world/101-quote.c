8045851d7a96e1571bb8a59d0f7f733d8ec75262
Cristian Granada <3451@holbertonschool.com>
2021-06-17 12:55:06 -0700

    2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return a string with her error
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char  name[5] = "Flavio";
+	return ("Size of %zu", name);
+}
