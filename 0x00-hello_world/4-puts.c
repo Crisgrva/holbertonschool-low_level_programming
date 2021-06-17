a7e6422cb86b7bb1ade7b88209cbc2a1a2bd550c
Cristian Granada <3451@holbertonschool.com>
2021-06-17 10:49:51 -0700

    fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-int main(void){puts("\"Programming is like building a multilingual puzzle"); return (0);}
+/**
+ * main - Entry point
+ * print string into puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
