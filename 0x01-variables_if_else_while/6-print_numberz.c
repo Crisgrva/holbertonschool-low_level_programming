40ab251b6716c26e01e775a4425ae95c9fa1ec9b
Cristian Granada <3451@holbertonschool.com>
2021-06-18 16:41:29 -0700

    task 6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbersz
+ * Print numbers from 0 to 9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number_0 = 48;
+	int number_9 = 57;
+
+	while (number_0 <= number_9)
+	{
+		putchar(number_0);
+		number_0++;
+	{
+	putchar('\n');
+	return (0);
+}
