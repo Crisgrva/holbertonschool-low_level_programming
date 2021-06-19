200e163699cf06a64470a1b5ab81aead1ec681ea
Cristian Granada <3451@holbertonschool.com>
2021-06-19 07:09:00 -0700

    task 100 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Comb3
+ * Print all combinations between 2 numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j = i + 1;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
