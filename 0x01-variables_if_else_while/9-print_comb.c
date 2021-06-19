424801231b53777d3dc73bad9db6956386d64053
Cristian Granada <3451@holbertonschool.com>
2021-06-18 17:07:22 -0700

    task 9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers
+ * Print numbers from 0 to 9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number_0 = 48;
+	int number_9 = 57;
+
+	while (number_0 <= number_9)
+	{
+		putchar(number_0);
+		if (number_0 < number_9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number_0++;
+	}
+	putchar('\n');
+	return (0);
+}
