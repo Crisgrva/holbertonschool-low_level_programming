b2991c7ba4e3b0b78bcfd524420d3fc7f2af52dd
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-23 13:27:39 -0500

    try task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,285 +2,45 @@
 #include "holberton.h"
 
 /**
- * _abs - Priunt absolute number
- * @n: number to evaluate
+ * times_table - Prints the 9 times table, starting with 0.
  * Return: Always 0 (Success)
  */
 
 void times_table(void)
 {
-	int n;
+	int i;
+	int j;
 
-	for (n = 0; n < 9; n++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-	}
-	_putchar('0');
-	_putchar('\n');
+		for (j = 0; j <= 9; j++)
+		{
+			int mul = i * j;
+			int mul_first = mul / 10;
+			int mul_second = mul % 10;
 
-	for (n = 0; n < 10; n += 1)
-	{
-		if (n > 9)
-		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-			_putchar(' ');
+			if (j == 0)
+			{
+				_putchar(mul_second + '0');
+				_putchar(',');
+			}
+			else if (mul <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul_second + '0');
+				if (j != 9)
+					_putchar(',');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(mul_first + '0');
+				_putchar(mul_second + '0');
+				if (j != 9)
+					_putchar(',');
+			}
 		}
-		else if (n == 9)
-		{
-			_putchar('0' + n);
-		}
-		else
-		{
-			_putchar('0' + n);
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 2); n += 2)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 18)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 3); n += 3)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 27)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 4); n += 4)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 36)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 5); n += 5)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 45)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 6); n += 6)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 54)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 7); n += 7)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 63)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 8); n += 8)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 72)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
-	}
-	_putchar('\n');
-
-	for (n = 0; n < (10 * 9); n += 9)
-	{
-		if (n == 0)
-		{
-			_putchar('0' + n);
-			_putchar(',');
-		}
-		else if (n == 81)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else if (n >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + n);
-			_putchar(',');
-		}
+		_putchar('\n');
 	}
 }
