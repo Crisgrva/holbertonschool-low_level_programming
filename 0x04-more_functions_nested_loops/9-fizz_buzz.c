af1c1c92b637fd6b134fac8e63f7e0b2e251e183
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-24 17:30:57 -0500

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 		if (i % 15 == 0)
 			printf("FizzBuzz ");
 
-		else if ((i % 3) == 0)
-			printf("Fizz ");
-
 		else if ((i % 5) == 0)
 			printf("Buzz ");
+
+		else if ((i % 3) == 0)
+			printf("Fizz ");
 
 		else
 			printf("%d ", i);
