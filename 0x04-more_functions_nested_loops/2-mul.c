8d12c72f4f4e11df693089a1beb486aff44f9a34
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-24 18:29:33 -0500

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* mul - multiplies two integers.
+* @a: digit to multiply
+* @b: digit to multiply
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
 int _isupper(char c);
 int _putchar(char c);
 int _isdigit(int c);
+int mul(int a, int b);
