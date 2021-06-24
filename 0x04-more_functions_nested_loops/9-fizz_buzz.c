644554c5d384b7e904c7a96e26b0efda3403685b
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-24 17:24:25 -0500

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - FizzBuzz
 * Return: 0
 */
 
 int main(void)
 {
 	int i;
-
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) || (i % 5)) == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
+
 		else if ((i % 3) == 0)
 			printf("Fizz ");
+
 		else if ((i % 5) == 0)
 			printf("Buzz ");
+
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+
 	return (0);
 }
