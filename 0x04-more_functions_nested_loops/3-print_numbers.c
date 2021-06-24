4d2e8caf0f2109c3f2fa32acca3c11ce97512848
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-24 18:33:00 -0500

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* print_numbers - print numbers from 0 to 9
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,3 +2,4 @@ int _isupper(char c);
 int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
