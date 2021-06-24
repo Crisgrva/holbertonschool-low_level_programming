d35184c7dd4be8a5f5ef69f1b5449cb1e8eaba73
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-24 18:22:51 -0500

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _isupper - Check is C is uppercase
+* @c: letter to evaluate
+* Return: 0
+*/
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,2 +1,3 @@
 int _isupper(char c);
 int _putchar(char c);
+int _isdigit(int c);
