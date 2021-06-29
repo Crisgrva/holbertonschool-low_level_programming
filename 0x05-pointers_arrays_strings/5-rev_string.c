f215b15a5ef596899d7c52af9c81c6670ce8df01
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-29 12:36:32 -0500

    task 5 done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include "stdio.h"
+
+/**
+* rev_string - function that returns string reversed
+* @s: string to reverse
+* Return: 0
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	int n = 0;
+	int j = 0;
+	int count = 0;
+	char width_var[1024];
+
+	/** Get lenght of string */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	count--;
+
+	/** init array with lenght of previous string */
+
+	for (; count >= -1; count--)
+	{
+		width_var[j] = s[count];
+		j++;
+	}
+
+	/** Reasign index of width array to s array */
+	while (s[n] != '\0')
+	{
+		s[n] = width_var[n];
+		n++;
+	}
+}
