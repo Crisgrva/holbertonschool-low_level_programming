cc5cea8f5e0cc48c4a8d8ea4ceb647ef17e6c60b
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-29 08:16:42 -0500

    try to fix task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@
 
 void print_rev(char *s)
 {
-	int i, max_index, prnt_str;
+	int i, max_index;
 
 	for (i = 0; s[i] != '\0'; i++)
 		max_index = i;
 
-	for (prnt_str = max_index; max_index != '\0'; max_index--)
+	for (; max_index != '\0'; max_index--)
 		_putchar(s[(max_index - 1)]);
 	_putchar('\n');
 }
