4743e609644e3ebb2600122ecc8f5c62d58cf9a3
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-29 17:06:06 -0500

    task 9 done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* *_strcpy - Copy string to another variable
+* @dest: destination of string
+* @src: source of string
+* Return: 0
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
