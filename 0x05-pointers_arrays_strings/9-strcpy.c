fddb037ec3fcf5dff5b8ca8696574f6d49c1563a
Cristian Granada <Chrisgrvc@gmail.com>
2021-06-29 17:13:50 -0500

    task 9 done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,8 +13,8 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		if (src[i] != '\0')
-			dest[i] = src[i];
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
