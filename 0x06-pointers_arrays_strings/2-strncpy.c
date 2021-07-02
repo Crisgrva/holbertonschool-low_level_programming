3653b4d5a57d7f5812a8a3be33ab3756212540e2
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-02 18:28:44 -0500

    task 2 done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - check the code for Holberton School students.
+ * @src: string to copy
+ * @dest: destiny
+ * @n: n bytes to copy
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int m;
+
+	for (m = 0; src[m] != '\0';)
+	{
+		m++;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < m)
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
+	return (dest);
+}
