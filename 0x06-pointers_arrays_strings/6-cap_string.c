4ebb66eda02c893635aff0ecf3b13c5ae73baadd
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-02 20:39:10 -0500

    task 6 done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - capitalizes all words of a string.
+ * @n: String to capitalize
+ * Return: string with all words capitalize.
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if ((n[i] >= 0 && n[i] <= 47) || (n[i] >= 123 && n[i] <= 126))
+		{
+			if (n[i] == 45)
+			{
+			}
+			else
+			{
+				i++;
+				if (n[i] >= 97 && n[i] <= 122)
+				{
+					n[i] = n[i] - 32;
+				}
+			}
+		}
+		if (n[i] == '\n' || n[i] == 32)
+		{
+			i++;
+			if (n[i] >= 97 && n[i] <= 122)
+			{
+				n[i] = n[i] - 32;
+			}
+		}
+	}
+	return (n);
+}
