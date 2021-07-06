f0c2951fa7b9b8879787619ab9d28367579bf62d
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-06 16:06:03 -0500

    try task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: matrix
+ * @size: size of matrix
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int n;
+	int area_size;
+	int first_element = 0;
+	int second_element = 0;
+
+	area_size = size * size;
+
+	for (i = 0; i <= area_size; i += (size + 1))
+	{
+		first_element += a[i];
+	}
+
+	for (n = 0; n <= area_size - size; n += (size - 1))
+	{
+		second_element += a[n];
+	}
+	printf("%d, %d\n", first_element, second_element);
+}
