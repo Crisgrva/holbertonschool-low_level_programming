df0e51e6301bbf25f66497d2678d7d7304542e1b
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-05 21:50:10 -0500

    task 3 done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 	int j;
 	int count = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != ' '; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
