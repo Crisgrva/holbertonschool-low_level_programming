ba6f2fd17290a1c4013c444b6b06836d9732d9ca
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-05 20:42:34 -0500

    task 2 done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] < c && s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
 	}
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (0);
+	return (0);
 }
