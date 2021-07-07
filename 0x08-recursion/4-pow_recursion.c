8106b5e417d1f347ed52c9803b73f75badf05a84
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-07 18:01:29 -0500

    task 4 done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * _pow_recursion: function that returns the value of x raised to the power of y.
+ * _pow_recursion - returns the value of x raised to the power of y.
  * @x: base number
  * @y: number of power
+ * Return: result of factiral operation
  */
 
 int _pow_recursion(int x, int y)
