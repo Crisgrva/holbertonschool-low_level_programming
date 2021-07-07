19f6de4b4830ba64e4d4a04947b70944e80e379f
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-07 11:49:06 -0500

    task 0 done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+	_putchar(s[i]);
+	i++;
+	if (s[i] != '\0')
+	{
+		_puts_recursion(&s[i]);
+	}
+}
