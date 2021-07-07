8c41a7563b9315f8c54f8ebe78d933911dccaedc
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-07 12:11:33 -0500

    task 0 done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,21 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != '\0')
-	{
-		_puts_recursion(s);
-	}
-	else if (*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		s++;
+		if (*s != '\0')
+		{
+			_puts_recursion(s);
+		}
+		else
+		{
+			_putchar('\n');
+		}
 	}
 }
