d206613162e880e746d1e8507b3facd485cb7284
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-07 19:14:43 -0500

    try 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * is_prime_number - returns prime number
+ * _sqrt_recursion - return square root of a number
  * @n: number to evaluate
  * Return: 1 if is primer of 0 is otherwise
  */
@@ -11,6 +11,12 @@ int _sqrt_recursion(int n)
 	return (_sqrt_recursion2(n, 1));
 }
 
+/**
+ * _sqrt_recursion2 - aux function
+ * @m: number from main function
+ * @i: number to evaluate if is square root
+ * Return: 1 if is primer of 0 is otherwise
+ */
 int _sqrt_recursion2(int m, int i)
 {
 	if (m < 0)
