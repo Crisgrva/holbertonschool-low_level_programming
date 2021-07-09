01aa75a85b8df7f9f14992ccbc8bf7f28d577a6f
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-09 15:14:44 -0500

    task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s", argv[i]);
+	printf("\n");
+}
