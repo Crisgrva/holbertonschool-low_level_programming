8ec6aad44d65bb5de80a569fc8ee5ccfdb030895
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-13 10:58:38 -0500

    fix task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc(size * sizeof(char));
+	s = (char *)malloc(size * sizeof(char));
 	if (size == 0)
 	{
 		return (NULL);
