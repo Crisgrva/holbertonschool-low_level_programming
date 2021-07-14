ad2b1937275d0e3cf2e7e4cde437cba34131405a
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-13 21:02:23 -0500

    try task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * free_grid - frees a 2 dimensional grid
  * @grid: grid to free memory
  * @height: height of grid
  * Return: Always 0.
  */
 
-void free_grid(int **grid, int height);
+void free_grid(int **grid, int height)
 {
 	int k;
 	for (k = 0; k < height; k++)
