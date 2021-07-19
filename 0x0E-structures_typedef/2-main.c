1c2b35c2b0e0e8be8af6cf12b590c6035591bcc9
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-19 14:47:04 -0500

    task 2

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	struct dog my_dog;
 
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	print_dog(NULL);
+	my_dog.name = NULL;
+	my_dog.age = 0;
+	my_dog.owner = NULL;
+	print_dog(&my_dog);
 	return (0);
 }
