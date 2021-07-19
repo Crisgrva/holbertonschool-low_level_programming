023711be534f7c9ac8c2bd1e749a289fd916c6e9
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-19 16:16:33 -0500

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,9 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 	my_dog->name = name;
-	my_dog->age = age;
 	my_dog->owner = owner;
 
+	free(my_dog);
 
 	return (my_dog);
 }
