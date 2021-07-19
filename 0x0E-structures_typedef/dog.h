d5dbbf6d15c3d85e4a933f632bf1535483a060a4
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-19 13:38:42 -0500

    add 0x0E and task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information
+ * @name: Dog's Name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+#endif /* DOG_H */
