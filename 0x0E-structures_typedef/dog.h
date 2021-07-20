e9968134721eb4b059fe9714b3dd0e596f8e4028
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-19 20:58:08 -0500

    task 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,4 +27,5 @@ dog_t *newBorn(char *name, int age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
