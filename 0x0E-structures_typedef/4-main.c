14f93461474c2eb638a81602175ff1f621585d76
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-20 06:50:21 -0500

    mergin from docker

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
@@ -10,7 +11,13 @@ int main(void)
 {
 	dog_t *my_dog;
 
-	my_dog = new_dog("Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	my_dog = new_dog("Ghost", 4.75, "Jon Snow");
+	if (my_dog == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	printf("My name is %s, I am %.2f, and my owner is %s\n",
+		   my_dog->name, my_dog->age, my_dog->owner);
 	return (0);
 }
