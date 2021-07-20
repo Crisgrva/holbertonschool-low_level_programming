0e7b4cd1863160b2eeae998e66bf8f6bcb7c72ae
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-20 06:57:55 -0500

    excluding e execut with gitignore

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,22 +15,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
 
-	if (!name || !owner || age < 0)
+	if (!my_dog || !name || !owner || age < 0)
 		return (NULL);
 
 	my_dog = malloc(sizeof(dog_t));
-	my_dog->name = strdup(name);
-	my_dog->owner = strdup(owner);
 
-	if (!my_dog || !my_dog->name || !my_dog->owner)
+	if (!my_dog)
 	{
-		free(my_dog->owner);
+		free(my_dog);
+		return (NULL);
+	}
+
+	my_dog->name = strdup(name);
+
+	if (!my_dog->name)
+	{
 		free(my_dog->name);
 		free(my_dog);
 		return (NULL);
 	}
 
 	my_dog->age = age;
+	my_dog->owner = strdup(owner);
 
+	if (!my_dog->owner)
+	{
+		free(my_dog->owner);
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
 	return (my_dog);
 }
