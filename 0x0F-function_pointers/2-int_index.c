2e09f6c6a76632796f9a9f3436acfc5a887416fb
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-21 21:47:00 -0500

    task 2 done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,17 +14,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	int count = 0;
 
 	if (!array || size <= 0 || !size || !cmp)
 		return (-1);
 
 	while (i < size)
 	{
-		(cmp)(array[i]);
-		if ((cmp)(array[i]))
-			count++;
+		if ((cmp)(array[i]) != 0)
+			return (i);
 		i++;
 	}
-	return (count);
+	return (-1);
 }
