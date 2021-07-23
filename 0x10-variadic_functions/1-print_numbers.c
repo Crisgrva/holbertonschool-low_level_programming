fb7393efac232086a3a43feec8fa26b94d933837
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-23 15:34:41 -0500

    add if n == 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap, int));
 
 		if (separator != NULL && i != (n - 1))
-			printf("%s ", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 
