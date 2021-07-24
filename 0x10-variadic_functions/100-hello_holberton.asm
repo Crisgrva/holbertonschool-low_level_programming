00aef4ed842bfc30529b8bf5f5691fe59dfb86b3
Cristian Granada <Chrisgrvc@gmail.com>
2021-07-24 10:12:08 -0500

    try 100

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,13 @@
+          global    main
+
+          section   .text
+main:     mov       rax, 1
+          mov       rdi, 1
+          mov       rsi, message
+          mov       rdx, 17
+          syscall
+          mov       rax, 60
+          xor       rdi, rdi
+          syscall
+          section   .data
+message:  db        "Hello, Holberton", 10
