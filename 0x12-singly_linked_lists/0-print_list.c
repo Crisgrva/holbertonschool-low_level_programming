78b741ae629a4d70ff284fbfd12467d8ab6c07a7
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-02 11:19:10 -0500

    fix printf in else task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("%i %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (length);
