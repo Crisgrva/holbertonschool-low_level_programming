8c8992e0d06e5a57e26f343cd86442fc95ddc742
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-02 12:16:50 -0500

    change comments of task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * list_len - function that prints all the elements of a list_t list.
+ * list_len - function that returns the number of elements in a linked list_t list.
  * @h: value of the leng str.
- * Return: Always 0.
+ * Return: lenght of linked list
  */
 
 size_t list_len(const list_t *h)
