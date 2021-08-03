1a6dd2f032128062bc3d27013d39833bc5166b48
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-03 17:40:07 -0500

    task 7 done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,13 +11,14 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
+	listint_t *current = head;
 
-	if (head == NULL)
+	if (current == NULL)
 		return (NULL);
 
 	for (count = 0; count < index; count++)
 	{
-		head = head->next;
+		current = current->next;
 	}
-	return (head);
+	return (current);
 }
