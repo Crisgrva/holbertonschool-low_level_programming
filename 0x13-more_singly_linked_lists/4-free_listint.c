5f0df9e175772417a4b9984076af6fd9d1e09ccb
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-03 11:55:00 -0500

    fix task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,10 +8,11 @@
 
 void free_listint(listint_t *head)
 {
+	listint_t *head_tmp;
 	while (!head)
 	{
-		free(head);
-		free(head->next);
+		head_tmp = head;
 		head = head->next;
+		free(head_tmp);
 	}
 }
