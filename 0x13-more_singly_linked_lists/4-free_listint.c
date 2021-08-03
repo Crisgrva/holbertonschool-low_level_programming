af7cd4d9a2d2fd46cc914d29a2276b2ddfbe6c42
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-03 11:40:14 -0500

    task 4 done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint - Write a function that frees a listint_t list.
+ * @head: head ref.
+ * Return: Always 0.
+ */
+
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head->next;
+	}
+}
