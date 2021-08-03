b6d5b91413986fcf8d222babdd220020a86530bf
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-03 11:11:12 -0500

    task 2 done

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - function that prints all the elements of a list_t list.
+ * @head: head ref.
+ * @n: value to insert.
+ * Return: Always 0.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	/* 1. allocate node */
+	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+
+	/* 2. put in the data  */
+	new_node->n = n;
+
+	/* 3. Make next of new node as head */
+	new_node->next = (*head);
+
+	/* 4. move the head to point to the new node */
+	(*head) = new_node;
+
+	return (new_node);
+}
