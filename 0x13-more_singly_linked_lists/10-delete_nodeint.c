efa4e14d5cd15fb7454ff88c38ea167f9993f6ec
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-03 20:52:16 -0500

    upload file task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Write a function that inserts a
+ * new node at a given position.
+ * @head: head ref.
+ * @index: idex of new node
+ * Return: 0
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	return (0);
+}
