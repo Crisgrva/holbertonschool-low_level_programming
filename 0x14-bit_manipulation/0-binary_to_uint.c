46adafbd450f5a241922672cb95d53b2d194be7d
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-05 16:03:37 -0500

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,13 +13,13 @@ unsigned int binary_to_uint(const char *b)
 	int i, j;
 	unsigned int result = 0;
 
-	if (!b)
+	if (!b || b == 0)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
 		if (i > 0)
-			pwd = pwd * 2;
+			pwd *= 2;
 
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
