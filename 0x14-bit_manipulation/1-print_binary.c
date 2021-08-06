379a3136a739f00e6757d6487feb5867eb0a70f8
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-06 05:46:00 -0500

    task 1 y 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_binary - Write a function that prints
  * the binary representation of a number.
  * @n: number to convert
  */
+
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
