815cc4e20f75641b88b03e7762607448ab4601ef
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-09 13:21:32 -0500

    add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
-* _strlen - function that returns the length of a string.
-* @s: variable to change the address
-* Return: 0
-*/
-
-int _strlen(char *s)
-{
-	int length = 0, i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		length++;
-
-	return (length);
-}
-
-/**
  * create_file - Create a function that creates a file.
  * @filename: Is the name of the file to create
  * @text_content: Is a NULL terminated string to write to the file.
@@ -25,19 +9,13 @@ int _strlen(char *s)
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, written, length;
+	int fd, written = 0, length = 0;
 
 	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT, 00600);
-		close(fd);
-		return (1);
-	}
-
-	length = _strlen(text_content);
+	while (text_content[length])
+		length++;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
 	if (fd < 0)
@@ -45,6 +23,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 		written = write(fd, text_content, length);
+
+	if (written < 0)
+		return (-1);
 
 	close(fd);
 	return (1);
