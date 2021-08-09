abe741a91311a84a4caaf2accc67be4e2275db4e
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-09 18:29:18 -0500

    add task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,36 +1,42 @@
 #include "main.h"
+/**
+ * main - Write a program that copies the
+ * content of a file to another file.
+ * @argc: arguments count
+ * @argv: Arguments i.e filenames
+ * Return: Always 0
+ */
 
 int main(int argc, char *argv[])
 {
-	/* File 1 */
-	int fd_1, read_1, length = 0;
-	char *buffer_1;
-
-	/* File 2 */
-	int fd_2, write_2;
-
-	/* Error Messages */
-	char usageERR[28] = "Usage: cp file_from file_to";
+	int fd_1, read_1, length = 0, close_1 = 0, fd_2, write_2, close_2 = 0;
+	char buffer_1[1024];
 
 	if (argc != 3)
-	{
-		write(1, usageERR, 28);
-		exit(97);
-	}
-
-	/* File 1 */
-	buffer_1 = malloc(sizeof(char) * 1024);
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
 	fd_1 = open(argv[1], O_RDONLY);
 	read_1 = read(fd_1, buffer_1, 1024);
-
+	if (fd_1 < 0)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 	while (buffer_1[length])
 		length++;
-
 	fd_2 = open(argv[2], O_CREAT | O_WRONLY, 0664);
 	write_2 = write(fd_2, buffer_1, length);
 
-	free(buffer_1);
-	close(fd_1);
-	close(fd_2);
+	if (fd_2 < 0 || write_2 != length)
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
+	if (close_1 < 0 || close_2 < 0)
+	{	close_1 = close(fd_1);
+		if (close_1 < 0)
+		{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_1);
+			exit(100); }
+		close_2 = close(fd_2);
+		if (close_2 < 0)
+		{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_2);
+			exit(100); }
+	}
 	return (0);
 }
