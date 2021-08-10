dd3765b671d87399eba3571c7749fce1f223d226
Cristian Granada <Chrisgrvc@gmail.com>
2021-08-10 18:30:20 -0500

    fix task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,19 +17,16 @@ int main(int argc, char *av[])
 
 	fd_1 = open(av[1], O_RDONLY);
 	if (fd_1 < 0)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	fd_2 = open(av[2], O_CREAT | O_WRONLY, 00664);
+	fd_2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
 
 	if (fd_2 < 0)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	while ((read_1 = read(fd_1, buffer_1, 1024)) > 0)
-	{
-		write_2 = write(fd_2, buffer_1, read_1);
-		if (write_2 < 0)
+		if ((write_2 = write(fd_2, buffer_1, read_1)) < 0)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	}
 
 	if (read_1 < 0)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
