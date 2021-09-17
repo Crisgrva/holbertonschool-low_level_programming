023cfaca40d3e44a4efcf329fd0ace144c261641
Crisgrva <crstngranada@gmail.com>
2021-09-17 17:43:23 -0500

    fix task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,17 +16,19 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->prev = last;
 
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
-		return (new_node);
 	}
+	else
+	{
+		while (last->next != NULL)
+			last = last->next;
 
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = new_node;
-	return (last->next);
+		last->next = new_node;
+		new_node->prev = last;
+	}
+	return (new_node);
 }
