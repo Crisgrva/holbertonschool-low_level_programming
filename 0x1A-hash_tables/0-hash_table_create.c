54abd8c334272795941dc08d41b901c7b75db339
Crisgrva <crstngranada@gmail.com>
2021-10-09 16:38:28 -0500

    fix betty task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int each_item = 0;
-
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
 	if (!table)
 	{
 		free(table);
